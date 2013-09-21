17ac1aa38c266630041b37fafd68f8986d2483ca
Chelsea Pugh <chelsea.pugh93@gmail.com>
2013-09-21 12:46:00 -0400

    changed

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 @app.route('/')
 def index():
 	obj = {
-		"title": "CHELSEACASSIEMOLLYCONNIEAWESOMEANDCASSIESSISTER",
+		"title": "Home Sweet Haus",
 		"text": "Now go hack!"
 	};
 	return render_template('index.jade', **obj)
